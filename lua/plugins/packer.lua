ab3609294c03c2a5b4f9ebd417126fe45838eabc
Vinny Meller <vinnymeller@gmail.com>
2022-10-26 19:56:14 -0500

    here sir

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -26,14 +26,7 @@ return require('packer').startup(function(use)
       "folke/noice.nvim",
       event = "VimEnter",
       config = function()
-        require("noice").setup({
-            messages = {
-                enabled = false,
-            },
-            popupmenu = {
-                enabled = false,
-            }
-        })
+        -- require("noice").setup()
       end,
       requires = {
         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
